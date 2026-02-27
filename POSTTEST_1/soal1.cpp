dd3e69b56c5be9efbeea79ddd9a5c7986d6f2cb5
intanayurahmaputri1@gmail.comgit config --global user.email intanayurahmaputri1@gmail.com <intanayurahmaputri1@gmail.com>
2026-02-27 15:56:34 +0800

    Revisi Post-Test SDA 1

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -25,7 +25,7 @@ Tmin(n) = C1 + C2 + C3n + C4n-C4 + 0 + C6n-C6 + C7n + C8+ C9
 b. Worst case: C1 + C2 + C3n + C4(n-1) + C5(n-1) + C6(n-1) + C7n + C8+ C9
 C5(n-1) => C5n-C5
 
-Maka Tmax(n) = C1 + C2 + C3n + C4n-C4 + C5n-C5 + C6n-C6 + C7n + C8+ C9
+Tmax(n) = C1 + C2 + C3n + C4n-C4 + C5n-C5 + C6n-C6 + C7n + C8+ C9
         = (C3+C4+C6+C5+C7)n + (C1+C2+C8+C9−C4−C6-C5)
         = (1+1+1+1+1)n + (1+1+1+1-1-1-1)
         = 5n+3, sama-sama membentuk fungsi linear, maka Big-O => O(n).
