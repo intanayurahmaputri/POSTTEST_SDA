836b871a19b1469fddfd7b73eb96a409d48ed5e7
intanayurahmaputri1@gmail.comgit config --global user.email intanayurahmaputri1@gmail.com <intanayurahmaputri1@gmail.com>
2026-02-27 20:09:04 +0800

    Revisi Post-Test SDA 1

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -1,47 +1,11 @@
 #include <iostream>
 using namespace std;
 
-/*
-• Pseudocode + Cost:
-procedure FindMin(A : array of n elements)
-min ← A[0]
-for i ← 1 to n-1 do
-    if A[i] < min then
-        min ← A[i]
-    end if
-end for
-return min    
-end procedure
-
-a. Best Case: C1 + C2 + C3n + C4(n-1) + 0 + C6(n-1) + C7n + C8+ C9
-C4 (n-1) => C4n-C4; C6 (n-1) => C6n-C6
-
-Tmin(n) = C1 + C2 + C3n + C4n-C4 + 0 + C6n-C6 + C7n + C8+ C9
-        = (C3+C4+C6+C7)n + (C1+C2+C8+C9−C4−C6)
-        = (1+1+1+1)n + (1+1+1+1-1-1)
-        = 4n+4, yang mana membentuk fungsi linear, karena fungsi linear maka Big-O => O(n).
-
-
-b. Worst case: C1 + C2 + C3n + C4(n-1) + C5(n-1) + C6(n-1) + C7n + C8+ C9
-C5(n-1) => C5n-C5
-
-Tmax(n) = C1 + C2 + C3n + C4n-C4 + C5n-C5 + C6n-C6 + C7n + C8+ C9
-        = (C3+C4+C6+C5+C7)n + (C1+C2+C8+C9−C4−C6-C5)
-        = (1+1+1+1+1)n + (1+1+1+1-1-1-1)
-        = 5n+3, sama-sama membentuk fungsi linear, maka Big-O => O(n).
-
-Big-O dari best case dan worst case:
-Best Case  : O(n)
-Worst Case : O(n)
-
-Hasil dari analisis menyatakan bahwa algoritma selalu linear, karena tetap membandingkan seluruh elemen.
-*/
-
 int FindMin(int A[], int n, int &index) {
     int min = A[0];
     index = 0;
 
-    for(int i = 1; i < n; i++) {
+    for(int i = 1; i < n; i++) {    
         if(A[i] < min) {
             min = A[i];
             index = i;
